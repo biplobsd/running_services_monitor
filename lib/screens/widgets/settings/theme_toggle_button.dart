e46b2d199cd6ce725840ba625dd0fb664d36010b
Fries_I23 <FriesI23@outlook.com>
2026-01-04 16:18:36 +0800

    feat: support follow system theme in theme toggle

--- lib/screens/widgets/settings/theme_toggle_button.dart
+++ lib/screens/widgets/settings/theme_toggle_button.dart
@@ -3,18 +3,51 @@ import 'package:running_services_monitor/core/dependency_injection/dependency_in
 import 'package:running_services_monitor/core/extensions.dart';
 import 'package:running_services_monitor/core/theme/theme_bloc.dart';
 
-class ThemeToggleButton extends StatelessWidget {
+class ThemeToggleButton extends StatefulWidget {
   const ThemeToggleButton({super.key});
 
   @override
+  State<ThemeToggleButton> createState() => _ThemeToggleButtonState();
+}
+
+class _ThemeToggleButtonState extends State<ThemeToggleButton> {
+  AppThemeMode getNextThemeMode(AppThemeMode themeMode, Brightness brightness) {
+    switch (themeMode) {
+      case AppThemeMode.light:
+        return brightness == Brightness.light
+            ? AppThemeMode.dark
+            : AppThemeMode.system;
+      case AppThemeMode.dark:
+        return brightness == Brightness.light
+            ? AppThemeMode.system
+            : AppThemeMode.light;
+      case AppThemeMode.system:
+        return brightness == Brightness.light
+            ? AppThemeMode.light
+            : AppThemeMode.dark;
+    }
+  }
+
+  void onPressed(Brightness brightness) {
+    setState(() {
+      final themeMode = getIt<ThemeBloc>().state;
+      final newThemeMode = getNextThemeMode(themeMode, brightness);
+      getIt<ThemeBloc>().add(ThemeEvent.setTheme(mode: newThemeMode));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final brightness = Theme.of(context).brightness; // Already cached effectively
-    final isDark = brightness == Brightness.dark;
+    final brightness = MediaQuery.platformBrightnessOf(context);
+    final themeMode = getIt<ThemeBloc>().state;
+    final icon = switch (themeMode) {
+      AppThemeMode.light => const Icon(Icons.light_mode),
+      AppThemeMode.dark => const Icon(Icons.dark_mode),
+      _ => const Icon(Icons.brightness_auto),
+    };
     return IconButton(
-      icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
-      onPressed: () {
-        getIt<ThemeBloc>().add(ThemeEvent.toggleTheme(isDark: isDark));
-      },
+      icon: icon,
+      onPressed: () => onPressed(brightness),
       tooltip: context.loc.toggleTheme,
     );
   }
