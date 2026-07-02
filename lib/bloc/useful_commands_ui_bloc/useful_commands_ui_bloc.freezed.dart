c136a08b045e6a18b2aa39fbc19b8553a4783709
Biplob Sutradhar <biplobsd11@gmail.com>
2026-07-02 13:44:50 +0600

    pkg update

--- lib/bloc/useful_commands_ui_bloc/useful_commands_ui_bloc.freezed.dart
+++ lib/bloc/useful_commands_ui_bloc/useful_commands_ui_bloc.freezed.dart
@@ -409,10 +409,7 @@ return $default(_that);case _:
 final _that = this;
 switch (_that) {
 case _UsefulCommandsUiState():
-return $default(_that);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that);}
 }
 /// A variant of `map` that fallback to returning `null`.
 ///
@@ -471,10 +468,7 @@ return $default(_that.showAddForm,_that.loadingCommandId);case _:
 @optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( bool showAddForm,  String? loadingCommandId)  $default,) {final _that = this;
 switch (_that) {
 case _UsefulCommandsUiState():
-return $default(_that.showAddForm,_that.loadingCommandId);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that.showAddForm,_that.loadingCommandId);}
 }
 /// A variant of `when` that fallback to returning `null`
 ///
