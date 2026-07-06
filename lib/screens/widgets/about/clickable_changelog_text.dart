1d22888294cb622977d7ffa66a26bfa4057568df
Biplob Sutradhar <biplobsd11@gmail.com>
2026-07-07 02:22:22 +0600

    changeslog ontap open release page and fix paddings

--- lib/screens/widgets/about/clickable_changelog_text.dart
+++ lib/screens/widgets/about/clickable_changelog_text.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:running_services_monitor/core/constants.dart';
 
@@ -13,67 +13,49 @@ class ClickableChangelogText extends StatelessWidget {
     await launchUrl(uri, mode: LaunchMode.externalApplication);
   }
 
+  String preprocessMarkdown(String rawText) {
+    final usernameRegex = RegExp(r'(?<=^|\s)@([a-zA-Z0-9-]+)');
+    final issueRegex = RegExp(r'(?<=^|\s)#([0-9]+)');
+
+    var processed = rawText.replaceAllMapped(usernameRegex, (match) {
+      final matchedText = match.group(0)!;
+      final username = match.group(1)!;
+      return '[$matchedText](https://github.com/$username)';
+    });
+
+    processed = processed.replaceAllMapped(issueRegex, (match) {
+      final matchedText = match.group(0)!;
+      final prNumber = match.group(1)!;
+      return '[$matchedText](${AppConstants.sourceCodeUrl}/issues/$prNumber)';
+    });
+
+    return processed;
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
-    final linkStyle = TextStyle(
-      color: colorScheme.primary,
-      fontWeight: FontWeight.bold,
-      decoration: TextDecoration.underline,
-    );
+    final processedText = preprocessMarkdown(text);
 
-    final List<InlineSpan> spans = [];
-    final regExp = RegExp(r'(@[a-zA-Z0-9-]+)|(#[0-9]+)');
-    
-    int lastMatchEnd = 0;
-    final matches = regExp.allMatches(text);
-
-    for (final match in matches) {
-      if (match.start > lastMatchEnd) {
-        spans.add(TextSpan(
-          text: text.substring(lastMatchEnd, match.start),
-        ));
-      }
-
-      final matchedText = match.group(0)!;
-      if (matchedText.startsWith('@')) {
-        final username = matchedText.substring(1);
-        final recognizer = TapGestureRecognizer()
-          ..onTap = () => launchBrowserUrl('https://github.com/$username');
-        spans.add(TextSpan(
-          text: matchedText,
-          style: linkStyle,
-          recognizer: recognizer,
-        ));
-      } else if (matchedText.startsWith('#')) {
-        final prNumber = matchedText.substring(1);
-        final recognizer = TapGestureRecognizer()
-          ..onTap = () => launchBrowserUrl('${AppConstants.sourceCodeUrl}/issues/$prNumber');
-        spans.add(TextSpan(
-          text: matchedText,
-          style: linkStyle,
-          recognizer: recognizer,
-        ));
-      }
-
-      lastMatchEnd = match.end;
-    }
-
-    if (lastMatchEnd < text.length) {
-      spans.add(TextSpan(
-        text: text.substring(lastMatchEnd),
-      ));
-    }
-
-    return RichText(
-      text: TextSpan(
-        style: TextStyle(
+    return MarkdownBody(
+      data: processedText,
+      onTapLink: (text, href, title) {
+        if (href != null) {
+          launchBrowserUrl(href);
+        }
+      },
+      styleSheet: MarkdownStyleSheet.fromTheme(theme).copyWith(
+        p: TextStyle(
           color: colorScheme.onSurface,
           fontSize: 13,
           height: 1.4,
         ),
-        children: spans,
+        a: TextStyle(
+          color: colorScheme.primary,
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.underline,
+        ),
       ),
     );
   }
